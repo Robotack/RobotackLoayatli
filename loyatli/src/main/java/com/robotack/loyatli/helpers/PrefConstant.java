3cff40b9638eee8760823bb1982c085c70b3db73
Moayad <mo2ed-pal@hotmail.com>
2021-08-18 23:05:52 +0300

    try

--- loyatli/src/main/java/com/robotack/loyatli/helpers/PrefConstant.java
+++
@@ -1,6 +0,0 @@
-package com.robotack.loyatli.helpers;
-
-public class PrefConstant {
-    public static final String custumerID = "custumerID";
-    public static final String identifierValue = "identifierValue";
-}
---
+++ loyatlisdk/src/main/java/com/robotack/loyatlisdk/LoyaltiStartAactivity.java
@@ -0,0 +1,22 @@
+package com.robotack.loyatlisdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoyaltiStartAactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loyalti_start_aactivity);
+
+        try {
+            Intent myIntent = new Intent(this,Class.forName(" com.robotack.loyalti.ui.Activites.LoyaltyActivity"));
+            startActivity(myIntent );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
