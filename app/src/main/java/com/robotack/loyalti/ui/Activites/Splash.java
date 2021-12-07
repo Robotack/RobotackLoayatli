0231a2aa57a00513c36c76e7bfa177040ccd672b
Moayad <mo2ed-pal@hotmail.com>
2021-12-07 22:10:08 +0200

    push

---
+++ app/src/main/java/com/robotack/loyalti/ui/Activites/Splash.java
@@ -0,0 +1,23 @@
+package com.robotack.loyalti.ui.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.robotack.loyalti.R;
+import com.robotack.loyalti.models.GetTokenListener;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        LoyaltyActivity.init(this, "UAT-00281253", "ar", new GetTokenListener() {
+            @Override
+            public String getToken() {
+                return "test";
+            }
+        });
+    }
+}
