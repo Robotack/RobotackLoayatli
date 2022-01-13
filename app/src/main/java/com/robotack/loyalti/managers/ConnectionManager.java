331e749a769c6059b92c0d159797cdb0e3f19b75
Moayed Alayaseh <mo2ed-pal@hotmail.com>
2022-01-13 11:13:28 +0200

    prod

--- app/src/main/java/com/robotack/loyalti/managers/ConnectionManager.java
+++ app/src/main/java/com/robotack/loyalti/managers/ConnectionManager.java
@@ -169,11 +169,11 @@ public class ConnectionManager {
     public interface APIService {
 
 //      public static String BASE_URL = "https://cbu-test.capitalbank.jo:8453/";
-      public static String BASE_URL = "https://cbu-test.capitalbank.jo:8453/api/v1.3/";
+//      public static String BASE_URL = "https://cbu-test.capitalbank.jo:8453/api/v1.3/";
 //      public static String BASE_URL = "https://loyalty-test.capitalbank.jo:8443/api/v1.3/";
 
 //         public static String BASE_URL = "https://robotack.au.ngrok.io/AdminPortal/api/v1.3/";
-//         public static String BASE_URL = "https://cbu.capitalbank.jo/api/v1.3/";
+         public static String BASE_URL = "https://cbu.capitalbank.jo/api/v1.3/";
 
 
         @Headers({"sdk_version:1","android_os_version:1"})
